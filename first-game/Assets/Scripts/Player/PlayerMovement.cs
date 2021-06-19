7a76d6510afe10015673c4543a8ee672007cbd15
Serhii Sharyi <serhiisharyi77@gmail.com>
2021-06-19 12:14:50 +0300

    New features
    - correct reaction while hitting to an obstacle
    - sideways movement
    
    Now player move forward by direct changing position, not by adding force

---
+++ first-game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+
+    public int forwardMovementSpeedPoints = 9;
+    public float sidewaysMovementSpeedNewtons = 500f;
+
+    private void FixedUpdate() {
+        forwardMovement();
+        movementLeftRight();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            forwardMovementSpeedPoints = 0;
+            sidewaysMovementSpeedNewtons = 0.000f;
+        }
+    }
+
+    private void movementLeftRight() {
+        rigidBody.AddForce(sidewaysMovementSpeedNewtons * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+    }
+
+    private void forwardMovement() {
+        transform.position += Vector3.forward * forwardMovementSpeedPoints * Time.deltaTime;
+    }
+}
